0a70ea1b751575c1c35690e26e189a0406436eee
Jalil Nouri <nouri.6412@gmail.com>
2021-07-26 16:30:01 +0430

    44

--- model/core.php
+++ model/core.php
@@ -9,16 +9,25 @@ class MBM_Ipak_Core
     {
         echo 'hello';
     }
+    public function define_bank()
+    {
+        
+    }
+    public function define_contact()
+    {
+        
+    }
+    public function define_cost()
+    {
+        
+    }
     public function menu()
     {
-        add_menu_page('سیستم حسابداری', ' حسابداری ایپک', 'manage_options', 'ipak-hesab-dashboard',array($this,"dashboard"), '');
-        add_submenu_page('ipak-hesab-dashboard', 'تعاریف', 'تعاریف', 'manage_options', 'ipak-hesab-dashboard', '');
-        add_submenu_page('ipak-hesab-dashboard', 'تعاریف بانک', 'تعاریف بانک', 'manage_options', 'ipak-hesab-define-bank', '');
-        add_submenu_page('ipak-hesab-dashboard', 'تعاریف طرف حساب', 'تعاریف طرف حساب', 'manage_options', 'ipak-hesab-define-contact', '');
-
-        add_submenu_page('ipak-hesab-dashboard', 'عملیات', 'عملیات', 'manage_options', 'ipak-hesab-operation', '');
-        add_submenu_page('ipak-hesab-operation', 'عملیات بانک', 'عملیات بانک', 'manage_options', 'ipak-hesab-operation-bank', '');
-        add_submenu_page('ipak-hesab-operation', 'عملیات طرف حساب', 'عملیات طرف حساب', 'manage_options', 'ipak-hesab-operation-contact', '');
+        add_menu_page('سیستم حسابداری', ' حسابداری ایپک', 'manage_options', 'ipak-hesab-dashboard',array($this,"dashboard"), 'dashicons-money-alt');
+        add_submenu_page('ipak-hesab-dashboard', 'داشبورد حسابداری', 'داشبورد حسابداری', 'manage_options', 'ipak-hesab-dashboard', array($this,"dashboard"));
+        add_submenu_page('ipak-hesab-dashboard', 'تعاریف بانک', 'تعاریف بانک', 'manage_options', 'ipak-hesab-define-bank', array($this,"define_bank"));
+        add_submenu_page('ipak-hesab-dashboard', 'تعاریف طرف حساب', 'تعاریف طرف حساب', 'manage_options', 'ipak-hesab-define-contact', array($this,"define_contact"));
+        add_submenu_page('ipak-hesab-dashboard', 'تعاریف هزینه', 'تعاریف هزینه', 'manage_options', 'ipak-hesab-define-cost',  array($this,"define_cost"));
 
     }
 }
