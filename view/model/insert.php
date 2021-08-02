883ab2789d22a1fb17230ae3cd29b1f9c961d847
Jalil Nouri <nouri.6412@gmail.com>
2021-08-02 15:14:02 +0430

    222

--- view/model/insert.php
+++ view/model/insert.php
@@ -8,5 +8,32 @@
             <div class="col-md-4">سلام</div>
             <div class="col-md-4">سلام</div>
         </div>
+        <button data-toggle="modal" data-target="#ipak-model-form" class="btn btn-primary">ایجاد</button>
+    </div>
+</div>
+
+<div class="modal fade" id="ipak-model-form" tabindex="-1" role="dialog" aria-labelledby="ipak-modal-title" aria-hidden="true">
+    <div class="modal-dialog modal-md modal-dialog-centered modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 id="ipak-modal-title" class="modal-title">فرم</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">بستن</button>
+            </div>
+
+        </div>
     </div>
 </div>
