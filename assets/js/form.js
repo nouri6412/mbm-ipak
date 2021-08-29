9973f0b8939b449539ad103ce4678341e298be24
Jalil Nouri <nouri.6412@gmail.com>
2021-08-29 15:56:02 +0430

    4

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,7 @@
 function ipak_hesab_model_form(model) {
     ipak_hesab_base_ajax({
         'action': 'ipak_hesab_model_form',
-        'model_name': model.model_name,
-        'model_id': model.model_id
+        'model': model
     }, function (result) {
         console.log(result);
     });
