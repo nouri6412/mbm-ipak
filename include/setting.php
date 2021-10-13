10603da9e907203942fd027a6063363dd34f4815
ieskandari <83347005+ieskandari@users.noreply.github.com>
2021-10-13 16:48:58 +0330

    tetytty

--- include/setting.php
+++ include/setting.php
@@ -1,7 +1,7 @@
 <?php
 class MBM_Ipak_Setting extends MBM_Ipak_Base_Class
 {
-
+    public $_woo_transition = "0";
 
     public function __construct()
     {
@@ -12,16 +12,35 @@ class MBM_Ipak_Setting extends MBM_Ipak_Base_Class
     {
        
         $this->post();
-
         $this->view('public/setting');
     }
 
     public function post()
     {
         global $MBM_Ipak_Core;
+        $page = '';
+        if (!empty($_REQUEST['page'])) {
+            $page = $_REQUEST['page'];
+        }   
         if (isset($_POST["submit_model"])) {
-            // $MBM_Ipak_Core->add_alert( "نباید خالی بماند", "success");
-            // $MBM_Ipak_Core->add_alert( "نباید خالی بماند", "danger");
+            
+            if (isset($_POST["chk-woo-to-ipak-sanad"])) {
+                
+                $search_main_table = sanitize_text_field($_POST["chk-woo-to-ipak-sanad"]);
+                if (sanitize_option($page . "_woo_transition", $search_main_table)) {
+                    update_option($page . "_woo_transition", $search_main_table);
+                    $MBM_Ipak_Core->add_alert( "تغییرات با موفقیت اعمال شد", "success");
+                }
+                else
+                {
+                    update_option($page . "_woo_transition", '0');
+                    $MBM_Ipak_Core->add_alert( "خطایی رخ داده است" , "danger");
+                }
+            }else{
+                update_option($page . "_woo_transition", '0');
+                $MBM_Ipak_Core->add_alert( "تغییرات با موفقیت اعمال شد", "success");
+            } 
         }
+        $this->_woo_transition = esc_sql(get_option($page . "_woo_transition"));
     }
 }
--- view/public/setting.php
+++ view/public/setting.php
@@ -4,6 +4,14 @@
     $MBM_Ipak_Core->print_alert(); ?>
 <form action="<?php  echo esc_html(admin_url('admin.php')); ?>?page=ipak-hesab-setting" method="POST">
 
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" <?php if($this->_woo_transition == "1") echo 'checked="checked"'; ?> value="1" name="chk-woo-to-ipak-sanad" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+            <?php  echo esc_html("پشتیبانی از تراکنش های افزونه ووکامرس"); ?>
+        </label>
+    </div>
+
+
 <input type="submit" name="submit_model" value="ذخیره"/>
 </form>
 </div>
