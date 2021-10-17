3cf508dd6b298a68592c79f9b00235ec1a382307
ieskandari <83347005+ieskandari@users.noreply.github.com>
2021-10-17 09:58:29 +0330

    serth

--- include/woocommerce.php
+++ include/woocommerce.php
@@ -22,7 +22,6 @@ class MBM_Ipak_Woocommerce extends MBM_Ipak_Base_Class
                 global $wpdb;
                 $table     = $wpdb->prefix . "hesab_sanad";
                 $description = sprintf( 'نام مشتری: %s %s شماره سفارش: %d' , $user->first_name , $user->last_name , $order_id );
-
                 $query_string       = $wpdb->prepare("insert into $table( `sanad_date`, `description`, `hesab_model_id`, `bed`, `bes`,`model_id`) VALUES (%s,%s, %d,%d,%d,%d)", array(date('Y-m-d'), $description, $order_id, $price, 0, -1));
                 $query_result       = $wpdb->query($query_string);
 
